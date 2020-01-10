3a3b6e53e8a17fc7483e4f673c3a1e1cb65c97e4
Ana Lucia <analuciat.12@gmail.com>
2020-01-09 22:19:20 -0300

    Publicação do Site

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php include "includes/header.php"?>
+
+
+<!-- Begin Body -->
+<body>
+
+    <!-- Begin Loader -->
+    <div class="loader" data-background-color="#ffffff" data-text-color="#000000">
+        <p>LOADING</p>
+        <span class="circle"></span>
+    </div>
+    <!-- End Loader -->
+
+    <!-- Begin Content -->
+    <div class="landing-slideshow">
+
+        <!-- Begin Logo container -->
+        <div class="logo-container">
+
+            <a href="#" class="source">
+
+                <img src="images/logo.png" alt="">
+
+                <span class="animate">
+                    <img src="images/logo.png" alt="">
+                </span>
+
+            </a>
+            
+        </div>
+        <!-- End Logo container -->
+
+
+        <!-- Begin Slides -->
+        <div class="flexslider">
+            <ul class="slides">
+                <li style="background-image:url(images/home/h1_1.jpg);"></li>
+                <li style="background-image:url(images/home/h1_2.jpg);"></li>
+                <li style="background-image:url(images/home/h1_3.jpg);"></li>
+                <li style="background-image:url(images/home/h1_4.jpg);"></li>
+                <li style="background-image:url(images/home/h1_8.jpg);"></li>
+                <li style="background-image:url(images/home/h1_9.jpg);"></li>
+                <li style="background-image:url(images/home/h1_10.jpg);"></li>
+                <li style="background-image:url(images/home/h1_11.jpg);"></li>
+                <li style="background-image:url(images/home/h1_12.jpg);"></li>
+                <li style="background-image:url(images/home/h1_14.jpg);"></li>
+            </ul>
+        </div>
+        <!-- End Slides -->
+
+    </div>
+    <!-- End Content -->
+
+
+<?php include "includes/footer.php"?>
